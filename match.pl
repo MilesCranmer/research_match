8bbd9c4194892c651f06ceafff7ac4bd56456f5c
mdtmc <miles.cranmer@gmail.com>
2016-12-26 17:20:07 -0500

    Switching to ADS API!

--- match.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-#File reads through abstracts to find matches
-use strict;
-use warnings;
-
-my $abstract_dir = "abstracts";
-my $name = 
---
+++ match.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import sys, os
+import ads as ads
+
+names_file = open(sys.argv[1])
+
+#Default abstract storage
+abstract_directory = "abstracts"
+if len(sys.argv) > 2:
+	abstract_directory = sys.argv[2]
+
+if not os.path.exists(abstract_directory):
+	os.makedirs(abstract_directory)
+
+number_abstracts = 4
+if len(sys.argv) > 3:
+	number_abstracts = int(sys.argv[3])
+
+
+i = 0
+for line in names_file:
+	#Only names
+	if line[0]==',': continue
+	if len(line) < 4: continue
+
+	cut_point = 0
+
+	#Find last space
+	for x in reversed(range(len(line))):
+		if line[x] == ' ':
+			cut_point = x
+			break
+	first_name = line[:x]
+	last_name = line[x+1:]
+	last_name = ''.join([char for char in last_name if char.isalpha()])
+
+	papers = ads.SearchQuery(
+		author=last_name+", "+first_name)
+	for paper in papers:
+		print paper.abstract
+	
+	#participant_file = open(abstract_directory+'/'+\
+		#first_name+'_'+last_name+'.txt', 'w')
+
+	#participant_file.write(html)
+
+	i+=1
+	if i > 4:
+		break
