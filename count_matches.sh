3b665d1235dd2160c7d60062bb593021ac552a9a
MilesCranmer <miles.cranmer@gmail.com>
2017-12-26 17:31:26 -0500

    Add more searches to count_matches.sh

--- count_matches.sh
+++ count_matches.sh
@@ -5,7 +5,7 @@
 Z=$(for x in $1/*\ *; do mv "$x" "${x// /_}"; done 2>/dev/null)
 
 # This shows the columns of our table
-echo "name, GPU, ML, Bayesian, Monte Carlo, NLP, python, C++"
+echo "name, GPU, ML, Bayesian, Monte Carlo, NLP, python, C++, dark matter, dark energy, surveys, lss, galaxy formation, statistics, pulsars, PCA"
 for x in $(ls -tr $1); do
     # This both searches for GPU in the $1, and prints the person's name
     grep -c -e "\<GPU\>" $1/$x | echo -n "$x, $(cat -), "
@@ -15,7 +15,15 @@ for x in $(ls -tr $1); do
     grep -c -e "MCMC" -e "Monte Carlo" $1/$x | echo -n "$(cat -), "
     # The "-i" flag here makes it case insensitive
     grep -c -i "python" $1/$x | echo -n "$(cat -), "
-    grep -c -i "C++" $1/$x | echo -n "$(cat -)"
+    grep -c -i "C++" $1/$x | echo -n "$(cat -), "
+    grep -c -i -e "dark matter" $1/$x | echo -n "$(cat -), "
+    grep -c -i -e "dark energy" $1/$x | echo -n "$(cat -), "
+    grep -c -i -e "survey" $1/$x | echo -n "$(cat -),"
+    grep -c -i -e "lss " -e "large scale structure" $1/$x | echo -n "$(cat -), "
+    grep -c -i -e "galaxy formation" $1/$x | echo -n "$(cat -), "
+    grep -c -i -e "statistics" $1/$x | echo -n "$(cat -), "
+    grep -c -i -e "pulsar" $1/$x | echo -n "$(cat -), "
+    grep -c -i -e "pca" -e "principal component analysis" $1/$x | echo -n "$(cat -)"
     # This final echo adds the newline
     echo ""
 done
